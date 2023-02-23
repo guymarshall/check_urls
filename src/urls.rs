bd595a32f3eb68c7184aa5ed3a56a3c087b55367
Guy Marshall <guymarshall9@gmail.com>
2023-02-23 22:28:13 +0000

    Add file writing and reading on ok() and err()

--- src/urls.rs
+++ src/urls.rs
@@ -1,17 +1,36 @@
 use reqwest::StatusCode;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 pub fn check_urls(urls: Vec<String>) {
+    let mut success_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("success.txt")
+        .unwrap();
+    let mut failure_file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("failure.txt")
+        .unwrap();
+
     for url in urls {
         match reqwest::blocking::get(&url) {
             Ok(response) => {
                 if response.status() == StatusCode::OK {
-                    println!("Success with {}", url);
+                    if let Err(e) = writeln!(success_file, "{}", url) {
+                        eprintln!("Error writing to success file: {}", e);
+                    }
                 } else {
-                    println!("Error with {}: {}", url, response.status());
+                    if let Err(e) = writeln!(failure_file, "{} - {}", url, response.status()) {
+                        eprintln!("Error writing to failure file: {}", e);
+                    }
                 }
             },
             Err(error) => {
-                println!("Error with {}: {}", url, error);
+                if let Err(e) = writeln!(failure_file, "{} - {}", url, error) {
+                    eprintln!("Error writing to failure file: {}", e);
+                }
             }
         }
     }
