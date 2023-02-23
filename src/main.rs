0c3fcba370ee0ba6e254a675385075b72fd6616b
Guy Marshall <guymarshall9@gmail.com>
2023-02-23 21:29:23 +0000

    Move check_urls to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,5 @@
 #![forbid(unsafe_code)]
 
-use reqwest::StatusCode;
-
-fn check_urls(urls: Vec<&str>) {
-    for url in urls {
-        match reqwest::blocking::get(url) {
-            Ok(response) => {
-                if response.status() == StatusCode::OK {
-                    println!("Success with {}", url);
-                } else {
-                    println!("Error with {}: {}", url, response.status());
-                }
-            },
-            Err(error) => {
-                println!("Error with {}: {}", url, error);
-            }
-        }
-    }
-}
-
 fn main() {
     let urls: Vec<&str> = vec!["https://google.co.uk", "https://notaewbsite.com"];
     check_urls(urls);
