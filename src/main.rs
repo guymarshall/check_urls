87c465563a9808ec941c035e604c9d612a919cb0
Guy Marshall <guymarshall9@gmail.com>
2023-02-23 21:36:48 +0000

    Add mod and use for input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 #![forbid(unsafe_code)]
 
 use urls::check_urls;
+use user_input::input;
 
 mod urls;
+mod user_input;
 
 fn main() {
+    let max: i32 = input("Enter max for IP check:");
     let urls: Vec<&str> = vec!["https://google.co.uk", "https://notaewbsite.com"];
     check_urls(urls);
 }
