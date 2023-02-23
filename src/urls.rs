cc9d759a9201d0d40f389fdce71c6fac2ff2b7f3
Guy Marshall <guymarshall9@gmail.com>
2023-02-23 22:39:09 +0000

    Add code to save current url to file and read it next time the program runs

--- src/urls.rs
+++ src/urls.rs
@@ -13,6 +13,13 @@ pub fn check_url(url: String) {
         .create(true)
         .open("failure.txt")
         .unwrap();
+    // Open the latest file with write access and truncate it
+    let mut latest_file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("latest.txt")
+        .unwrap();
 
     match reqwest::blocking::get(&url) {
         Ok(response) => {
@@ -32,19 +39,40 @@ pub fn check_url(url: String) {
             }
         }
     }
+
+    // Write the latest URL to the latest file
+    if let Err(e) = writeln!(latest_file, "{}", url) {
+        eprintln!("Error writing to latest file: {}", e);
+    }
 }
 
 pub fn ip_addresses(max: i32) {
-    for i in 0..max {
-        for j in 0..max {
-            for k in 0..max {
-                for l in 0..max {
+    let latest_url = std::fs::read_to_string("latest.txt").unwrap_or("0.0.0.0".to_string());
+
+    let latest_parts: Vec<&str> = latest_url.split('.').collect();
+    let mut i = latest_parts[0].parse::<i32>().unwrap();
+    let mut j = latest_parts[1].parse::<i32>().unwrap();
+    let mut k = latest_parts[2].parse::<i32>().unwrap();
+    let mut l = latest_parts[3].parse::<i32>().unwrap();
+
+    for _ in i..max {
+        for _ in j..max {
+            for _ in k..max {
+                for _ in l..max {
                     let url = format!("http://{}.{}.{}.{}", i, j, k, l);
                     check_url(url);
                     let url = format!("https://{}.{}.{}.{}", i, j, k, l);
                     check_url(url);
+
+                    l += 1;
                 }
+                l = 0;
+                k += 1;
             }
+            k = 0;
+            j += 1;
         }
+        j = 0;
+        i += 1;
     }
 }
