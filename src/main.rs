24f9ee6d56b20c48b289f8f785dd80bb383f1dae
Guy Marshall <guymarshall9@gmail.com>
2023-02-23 21:33:49 +0000

    Add mod for urls and use for check_urls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![forbid(unsafe_code)]
 
+use urls::check_urls;
+
+mod urls;
+
 fn main() {
     let urls: Vec<&str> = vec!["https://google.co.uk", "https://notaewbsite.com"];
     check_urls(urls);
