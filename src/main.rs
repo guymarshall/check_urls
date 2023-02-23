24f8fe0838d79ba50636884818e41b341855e759
Guy Marshall <guymarshall9@gmail.com>
2023-02-23 20:44:24 +0000

    Add check_urls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use reqwest::StatusCode;
+
+fn check_urls(urls: Vec<&str>) {
+    for url in urls {
+        match reqwest::blocking::get(url) {
+            Ok(response) => {
+                if response.status() == StatusCode::OK {
+                    println!("Success with {}", url);
+                } else {
+                    println!("Error with {}: {}", url, response.status());
+                }
+            },
+            Err(error) => {
+                println!("Error with {}: {}", url, error);
+            }
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
