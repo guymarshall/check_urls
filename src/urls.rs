f444eab730c9f08716c7f5d229546c2f0abbcdcf
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2023-02-24 12:42:47 +0000

    Change 10 millis to 5 secs to stop false timed outs

--- src/urls.rs
+++ src/urls.rs
@@ -19,7 +19,7 @@ fn check_url(url: String) {
         .unwrap();
 
     let client = blocking::Client::builder()
-        .timeout(Duration::from_millis(10))
+        .timeout(Duration::from_secs(5))
         .build()
         .unwrap();
 
